d6a0da8c1942c9b160f5175541960a0494e9a54a
angel-geyuwen <2190162725@qq.com>
2017-09-07 11:21:04 +0000

    second

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import  img from './images/log.jpg'
 import  img1 from './images/baggage.jpg'
+import  Logo from './Logo/Logo'
+import  Logo1 from './Logo/Logo1'
 class App extends Component {
   render() {
     return (
