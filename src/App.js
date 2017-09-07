69b7fb227e71a8a377b4babe35bccb273b6d4aef
Peter Wang <happypeter1983@gmail.com>
2017-09-07 12:08:12 +0800

    fix svg bug

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import  img from './images/log.jpg'
 import  img1 from './images/baggage.jpg'
-import  Logo from './Logo/Logo'
+import three from './three.svg'
 import  Logo1 from './Logo/Logo1'
 class App extends Component {
   render() {
     return (
       <div className='header'>
       <div className="top">
-      <a className="Logo" href='#'><Logo color='#ccc' width='28px' height='36px'></a>
-      <img src={img} alt="" className="log"></img>
+      <a className="logo" href='#'>
+        <img src={three} alt="logo"/>
+      </a>
       <a href="javascript:;" className="sousuo"><i className="iconfont">&#xe607;</i></a>
       <img src={img1} alt="" className="baggage"></img>
       <span className="first"><a href="">登陆/</a></span>
